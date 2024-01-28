c12aa36a3e0238ff6e58eb29f72704b3fe064ee5
hitenjaypal <jaypalhiten144@gmail.com>
2024-01-28 22:38:19 +0530

    1.DONE

---
+++ 1 Basics Problems/f.java
@@ -0,0 +1,34 @@
+// 6) Write a Program to Swap Two Numbers
+
+public class f {
+    public static void main(String[] args) {
+        
+        int a = 6;
+        int b =4;
+        int temp;
+
+        //swaping the value [with temp variable]
+
+        temp=a;
+        a=b;
+        b=temp;
+
+        // System.out.println("The values of a and b after swapping are : "+ "a="+a +"and b="+b);
+        System.out.println("The value of a after Swapping:"+ a);
+        System.out.println("The value of b after Swapping:"+ b);
+    
+         
+        
+        //swaping the value [without temp variable]
+         
+        //  a=a+b;   // a=10
+        //  b=a-b;   // b=10-4=6
+        //  a=a-b;   // a=10-6=4
+
+        //  System.out.println("The value of a after Swapping:"+ a);
+        //  System.out.println("The value of b after Swapping:"+ b);
+     
+
+
+}
+}
---
+++ 1 Basics Problems/h.java
@@ -0,0 +1,22 @@
+// 8) Write a Program to Multiply two decimal Numbers entered by User
+
+import java.util.*;
+
+public class h {
+     
+        
+    public static void main(String[] args) {
+        
+
+        System.out.println("Enter the first number:");
+        Scanner sc = new Scanner(System.in);
+        float num1 = sc.nextFloat();
+        System.out.println("Enter the first number:");
+        float num2 = sc.nextFloat();
+
+        float Result = num1 * num2 ;
+        System.out.println("Multiply of two decimal Numbers are:" + Result);
+
+}
+
+}
