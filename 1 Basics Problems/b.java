9a686d012cc0dc4076fd8a461093e8040dd1896c
Hiten Jaypal <100457414+hitenjaypal@users.noreply.github.com>
2024-01-29 00:20:22 +0530

    Add files via upload

---
+++ 1 Basics Problems/b.java
@@ -0,0 +1,13 @@
+//2) Write a Program to Print Integer Number Entered by User
+
+import java.util.Scanner;
+
+public class b {
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("Enter an integer number:");
+        int n = sc.nextInt();
+        System.out.println("The entered integer is : " +n);
+    
+    }
+}
---
+++ 1 Basics Problems/c.java
@@ -0,0 +1,32 @@
+// 3) Write a Program to Add Two Integer Numbers Entered by user
+
+
+// 1.Print by User
+
+// import java.util.*;
+
+// public class c {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter a:");
+//         int a = sc.nextInt();
+//         System.out.println("Enter b:");
+//         int b = sc.nextInt();
+//         int x = a + b;
+//         System.out.println("The sum of a & b is:" + x);
+
+
+//     }
+// }
+
+// 2.By self 
+
+public class c {
+    public static void main(String[] args) {
+        int a = 5;
+        int b =9;
+        int x = a + b;
+        System.out.println("The sum of a & b is:" + x);
+    }
+}
+
