a51ec7de8a6ab557aa779f5ffefe8f293e125be1
Hiten Jaypal <100457414+hitenjaypal@users.noreply.github.com>
2024-01-31 22:03:53 +0530

    Add files via upload

---
+++ 5Patterns/a.java
@@ -0,0 +1,21 @@
+// 1) Solid Rectangular star
+// * * * * *
+// * * * * *
+// * * * * *
+
+
+public class a {
+    public static void main(String[] args) {
+        int n = 2;
+        int m =5;
+
+        for (int i = 0; i <= n; i++) {
+
+            for (int j = 0; j <= m; j++) {
+
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
